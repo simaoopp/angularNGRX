41b953ae96d74b64e6b30b15edcdac56352a2f33
simaoopp <simaopereira308@gmail.com>
2023-05-19 13:38:30 +0100

    add file angularNGRXapp/

--- angularNGRXapp/src/app/app.module.ts
+++ angularNGRXapp/src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./books/books.module').then((b) => b.BooksModule),
+  },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
